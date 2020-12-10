18a37db92b5c3702625b2877f6eae4d3b24d37f1
Anna Naydenova <anna.naydenova@gmail.com>
2020-12-10 19:00:14 +0100

    finished exercise

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,9 +6,9 @@ class ReviewsController < ApplicationController
   end
 
   def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
     # we need `restaurant_id` to associate review with corresponding restaurant
-    @restaurant = Restaurant.find(params[:restaurant_id])
     @review.restaurant = @restaurant
     if @review.save
       redirect_to restaurant_path(@restaurant)
@@ -17,18 +17,15 @@ class ReviewsController < ApplicationController
     end
   end
 
-
   def destroy
     @review = Review.find(params[:id])
     @review.destroy
     redirect_to restaurant_path(@review.restaurant)
   end
 
-
   private
 
   def review_params
-    params.require(:review).permit(:content)
+    params.require(:review).permit(:content, :rating)
   end
-
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
 
-  validates :content, presence: true
+  validates :content, :rating, presence: true
+  validates :rating, inclusion: { in: 0..5, message: '%{value} is not a valid rating'}, numericality: { only_integer: true }
 end
